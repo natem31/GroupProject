765c8c949af66e93478391af89a3861b5411ecc1
natem31 <nmueller1@babson.edu>
2025-05-03 19:15:02 -0400

    Scheduler Web App

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv()  # Loads from .env into environment
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///scheduler.db'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
